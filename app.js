95ed79600e516f371bb36e758d00601daa8cfabe
erininabox <erininabox@gmail.com>
2021-07-14 17:03:50 -0700

    turns, win conditions

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 // DOM References
-const gameBoardEl = document.querySelector(".grid-container"); //change this to whatever you named yours
+const boardElement = document.querySelector(".board");
+const turnAnnounce = document.querySelector(".turn-announce");
+const winAnnounce = document.querySelector(".win-announce");
+const square1a = document.querySelector(".square1a");
+const square1b = document.querySelector(".square1b");
+const square1c = document.querySelector(".square1c");
+const square2a = document.querySelector(".square2a");
+const square2b = document.querySelector(".square2b");
+const square2c = document.querySelector(".square2c");
+const square3a = document.querySelector(".square3a");
+const square3b = document.querySelector(".square3b");
+const square3c = document.querySelector(".square3c");
+
 
 // GLOBAL VARIABLES (aka State)
 let currentPlayer = 'x'; // this is either x or o
@@ -14,10 +26,75 @@ let currentPlayer = 'x'; // this is either x or o
 
 function handleClick(event) {
     let clickedSquare = event.target;
-    console.log(clickedSquare.id);
+    
+    // Checks for filled squares
+    if (clickedSquare.classList.contains("x")) {
+        turnAnnounce.textContent = "That square is taken. Try another square.";
+        return null;
+    };
+    if (clickedSquare.classList.contains("o")) {
+        turnAnnounce.textContent = "That square is taken. Try another square.";
+        return null;
+    };
+    
+    // Denotes who is the current player w/ class
+    clickedSquare.classList.add(currentPlayer)
+    winner();
+
+    // Sets the announcement of whose turn it is and switches the turn
+    if (currentPlayer == "x") {
+        currentPlayer = "o";
+        turnAnnounce.textContent = "It is O's turn!";
+    } else {
+        currentPlayer = "x";
+        turnAnnounce.textContent = "It is X's turn!";
+    };
+    
+};
+
+function winner (){
+    let winMessageX = "X wins this game!";
+    let winMessageO = "O wins this game!";
+    let tieMessage = "It's a tie!";
+
+    if (square1a.classList.contains("x") && square1b.classList.contains("x") && square1c.classList.contains("x")) {
+        winAnnounce.textContent = winMessageX;
+        winAnnounce.style.backgroundColor = "pink";
+    } else if (square2a.classList.contains("x") && square2b.classList.contains("x") && square2c.classList.contains("x")) {
+        winAnnounce.textContent = winMessageX;
+        winAnnounce.style.backgroundColor = "pink";
+    } else if (square3a.classList.contains("x") && square3b.classList.contains("x") && square3c.classList.contains("x")) {
+        winAnnounce.textContent = winMessageX;
+        winAnnounce.style.backgroundColor = "pink";
+    } else if (square1a.classList.contains("x") && square2b.classList.contains("x") && square3c.classList.contains("x")) {
+        winAnnounce.textContent = winMessageX;
+        winAnnounce.style.backgroundColor = "pink";
+    } else if (square3a.classList.contains("x") && square2b.classList.contains("x") && square1c.classList.contains("x")) {
+        winAnnounce.textContent = winMessageX;
+        winAnnounce.style.backgroundColor = "pink";
+    // } else if (){
+    //     winAnnounce.textContent = tieMessage;
+    } else if (square1a.classList.contains("o") && square1b.classList.contains("o") && square1c.classList.contains("o")) {
+        winAnnounce.textContent = winMessageO;
+        winAnnounce.style.backgroundColor = "pink";
+    } else if (square2a.classList.contains("o") && square2b.classList.contains("o") && square2c.classList.contains("o")) {
+        winAnnounce.textContent = winMessageO;
+        winAnnounce.style.backgroundColor = "pink";
+    } else if (square3a.classList.contains("o") && square3b.classList.contains("o") && square3c.classList.contains("o")) {
+        winAnnounce.textContent = winMessageO;
+        winAnnounce.style.backgroundColor = "pink";
+    } else if (square1a.classList.contains("o") && square2b.classList.contains("o") && square3c.classList.contains("o")) {
+        winAnnounce.textContent = winMessageO;
+        winAnnounce.style.backgroundColor = "pink";
+    } else if (square3a.classList.contains("o") && square2b.classList.contains("o") && square1c.classList.contains("o")) {
+        winAnnounce.textContent = winMessageO;
+        winAnnounce.style.backgroundColor = "pink";
+    }
+    else {
+        winAnnounce.textContent = '';
+    };
 };
 
 // EVENT LISTENERS
-// "Event Delegation" allows for us to get away with just one event listener instead of nine
 
-gameBoardEl.addEventListener('click', handleclick);
+boardElement.addEventListener('click', handleClick);
